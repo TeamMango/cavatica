5e7b449d86a537a1c83bafbaec6ebbc743096fa7
upsresearch2 <41304354+upsresearch2@users.noreply.github.com>
2018-07-25 18:55:47 -0400

    Updated to Google Style Guide

--- code/script.sh
+++ code/script.sh
@@ -2,12 +2,13 @@
 # Auth: Jennifer Chang
 # Date: 2018/04/23
 # Desc: Run a search term
+# Copyright:
 
 set -e
 set -u
 
 # Place search terms in config.txt
-ARR=(`grep -v "^term" config.txt |awk -F',' '{print $1}'|tr '\n' ' '`)
+ARR=$(grep -v "^term" config.txt |awk -F',' '{print $1}'|tr '\n' ' ')
 CODEDIR=../../code
 
 touch logfile.txt
@@ -15,28 +16,27 @@ date
 date >> logfile.txt
 
 # ================================ PubMed Search
-for TERM in "${ARR[@]}"
-do
-    echo -e "===== ${TERM}\n"
+for TERM in "${ARR[@]}"; do
+  echo -e "===== ${TERM}\n"
 
-    echo -e "===== Search ${TERM} in PubMed\n"
-#    echo "perl ${CODEDIR}/searchPubmedBtwnPubDates.pl ${TERM} ${TERM}_pm.ids"
-#    [[ -f "${TERM}_pm.ids" ]] || perl ${CODEDIR}/searchPubmedBtwnPubDates.pl ${TERM} ${TERM}_pm.ids
-    echo "bash ${CODEDIR}/pubmed_ids.sh ${TERM} > ${TERM}_pm.ids"
-    [[ -f "${TERM}_pm.ids" ]] || bash ${CODEDIR}/pubmed_ids.sh ${TERM} > ${TERM}_pm.ids
-    echo "${CODEDIR}/pubmed_xml.sh ${TERM}_pm.ids > ${TERM}_pm.xml"
-    [[ -f "${TERM}_pm.xml" ]] || ${CODEDIR}/pubmed_xml.sh ${TERM}_pm.ids > ${TERM}_pm.xml
+  echo -e "===== Search ${TERM} in PubMed\n"
+  #    echo "perl ${CODEDIR}/searchPubmedBtwnPubDates.pl ${TERM} ${TERM}_pm.ids"
+  #    [[ -f "${TERM}_pm.ids" ]] || perl ${CODEDIR}/searchPubmedBtwnPubDates.pl ${TERM} ${TERM}_pm.ids
+  echo "bash ${CODEDIR}/pubmed_ids.sh ${TERM} > ${TERM}_pm.ids"
+  [[ -f "${TERM}_pm.ids" ]] || bash ${CODEDIR}/pubmed_ids.sh ${TERM} > ${TERM}_pm.ids
+  echo "${CODEDIR}/pubmed_xml.sh ${TERM}_pm.ids > ${TERM}_pm.xml"
+  [[ -f "${TERM}_pm.xml" ]] || ${CODEDIR}/pubmed_xml.sh ${TERM}_pm.ids > ${TERM}_pm.xml
 
-    # Visual check, manual curation
-    query=`echo $TERM |sed 's/+/ /g'`
-    [[ -f "${TERM}_pm.html" ]] || perl ${CODEDIR}/pubmed_text_analyzer2.pl "$query" ${TERM}_pm.xml > ${TERM}_pm.html
+  # Visual check, manual curation
+  query=$(echo $TERM |sed 's/+/ /g')
+  [[ -f "${TERM}_pm.html" ]] || perl ${CODEDIR}/pubmed_text_analyzer2.pl "$query" ${TERM}_pm.xml > ${TERM}_pm.html
 	
-    echo "===== Generate authors and paper lists"
-    [[ -f "${TERM}_authors_pm.tsv" ]] || ${CODEDIR}/authorlist_pm.pl ${TERM}_pm.xml > ${TERM}_authors_pm.tsv
-    [[ -f "${TERM}_papers_pm.tsv" ]] || ${CODEDIR}/paperlist_pm.pl ${TERM}_pm.xml > ${TERM}_papers_pm.tsv
+  echo "===== Generate authors and paper lists"
+  [[ -f "${TERM}_authors_pm.tsv" ]] || ${CODEDIR}/authorlist_pm.pl ${TERM}_pm.xml > ${TERM}_authors_pm.tsv
+  [[ -f "${TERM}_papers_pm.tsv" ]] || ${CODEDIR}/paperlist_pm.pl ${TERM}_pm.xml > ${TERM}_papers_pm.tsv
 
-    echo "===== Generate Paper Count barcharts"
-    [[ -f "${TERM}_pm.png" ]] || ${CODEDIR}/mkBarchart.R ${TERM}_papers_pm.tsv ${TERM}_pm.png 1996 2018
+  echo "===== Generate Paper Count barcharts"
+  [[ -f "${TERM}_pm.png" ]] || ${CODEDIR}/mkBarchart.R ${TERM}_papers_pm.tsv ${TERM}_pm.png 1996 2018
 done
 
 wc -l *pm.ids >> logfile.txt
@@ -57,22 +57,20 @@ perl ${CODEDIR}/hack.pl trends-temp3.txt
 perl ${CODEDIR}/hack.pl trends-temp3.txt > trends_pm.txt
 
 # ================================ PMC Search
-for TERM in "${ARR[@]}"
-do
-    echo -e "===== Search ${TERM} in PMC\n"
-    [[ -f "${TERM}_pmc.ids" ]] ||perl ${CODEDIR}/searchPMCBtwnPubDates.pl ${TERM} ${TERM}_pmc.ids
-    [[ -f "${TERM}_pmc.xml" ]] || echo "${CODEDIR}/pmc_xml.sh ${TERM}_pmc.ids > ${TERM}_pmc.xml"
-    [[ -f "${TERM}_pmc.xml" ]] || ${CODEDIR}/pmc_xml.sh ${TERM}_pmc.ids > ${TERM}_pmc.xml
-    query=`echo $TERM |sed 's/+/ /g'`
-    [[ -f "${TERM}_pmc.hmtl" ]] || ${CODEDIR}/pubmed_fulltext_analyzer_v4.pl ${query} ${TERM}_pmc.xml > ${TERM}_pmc.html
-    
-    echo "===== Generate authors and paper lists"
-    KEYWORD=`echo $TERM | sed 's/+/ /g'`
-    [[ -f "${TERM}_authors_pmc.tsv" ]] || ${CODEDIR}/bothlist_pmc.pl "${KEYWORD}" ${TERM}_papers_pmc.tsv ${TERM}_authors_pmc.tsv ${TERM}_pmc.xml 2> /dev/null # silence incomplete author information
+for TERM in "${ARR[@]}"; do
+  echo -e "===== Search ${TERM} in PMC\n"
+  [[ -f "${TERM}_pmc.ids" ]] ||perl ${CODEDIR}/searchPMCBtwnPubDates.pl ${TERM} ${TERM}_pmc.ids
+  [[ -f "${TERM}_pmc.xml" ]] || echo "${CODEDIR}/pmc_xml.sh ${TERM}_pmc.ids > ${TERM}_pmc.xml"
+  [[ -f "${TERM}_pmc.xml" ]] || ${CODEDIR}/pmc_xml.sh ${TERM}_pmc.ids > ${TERM}_pmc.xml
+  query=$(echo $TERM |sed 's/+/ /g')
+  [[ -f "${TERM}_pmc.hmtl" ]] || ${CODEDIR}/pubmed_fulltext_analyzer_v4.pl ${query} ${TERM}_pmc.xml > ${TERM}_pmc.html
+  
+  echo "===== Generate authors and paper lists"
+  KEYWORD=$(echo $TERM | sed 's/+/ /g')
+  [[ -f "${TERM}_authors_pmc.tsv" ]] || ${CODEDIR}/bothlist_pmc.pl "${KEYWORD}" ${TERM}_papers_pmc.tsv ${TERM}_authors_pmc.tsv ${TERM}_pmc.xml 2> /dev/null # silence incomplete author information
 
-    
-    echo "===== Generate Paper Count barcharts"
-    [[ -f "${TERM}_pmc.png" ]] || ${CODEDIR}/mkBarchart.R ${TERM}_papers_pmc.tsv ${TERM}_pmc.png 1996 2018
+  echo "===== Generate Paper Count barcharts"
+  [[ -f "${TERM}_pmc.png" ]] || ${CODEDIR}/mkBarchart.R ${TERM}_papers_pmc.tsv ${TERM}_pmc.png 1996 2018
 done
 
 wc -l *pmc.ids >> logfile.txt
